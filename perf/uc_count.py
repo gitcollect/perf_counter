0d140de6c95afed3ebfc2b2c15feaa3ef26e2574
root <root@nebula2.com>
2015-07-08 15:19:08 -0400

    ucevent

--- perf/uc_count.py
+++ perf/uc_count.py
@@ -2,17 +2,8 @@
 # File Name: perf_count.py
 
 """
-	This file is used to count the uncore performance events
-	This is not finished. But it does not support process-wide. 
-	events:
-		CAS_COUNT.RD: uncore_imc_X/event=0x4,umask=0x3/
-		CAS_COUNT.WR: uncore_imc_X/event=0x4,umask=0xc/
-		PRE_COUNT.PAGE_MISS: uncore_imc_X/event=0x2,umask=0x1/
-		ACT_COUNT: uncore_imc_0/event=0x1/
-	Calculation:
-		iMC.PCT_REQUESTS_PAGE_MISS = PRE_COUNT.PAGE_MISS / (CAS_COUNT.RD + CAS_COUNT.WR)
-		iMC.PCT_REQUESTS_PAGE_EMPTY = (ACT_COUNT - PRE_COUNT.PAGE_MISS)/ (CAS_COUNT.RD + CAS_COUNT.WR)
-		iMC.PCT_REQUESTS_PAGE_HIT = 1 - iMC.PCT_REQUESTS_PAGE_MISS - iMC.PCT_REQUESTS_PAGE_EMPTY
+	This file is used to count and calculate the uncore performance events
+	Uncore events can only be supported system-wide.
 """
 
 import os
@@ -22,11 +13,10 @@ import getopt
 import locale
 import argparse
 
-PID = ""
 CONCURRENCY = 0
-ROUND_NR = 0
 INTERVAL = ""
-OPTIONS = ""
+TOTAL_TIME = ""
+SOCKET_NR = 2
 
 event_list = []
 event_value = []
@@ -35,41 +25,20 @@ def median(lst):
 	lst.sort()
         return lst[len(lst)/2]
 
-def init_event_list():
-	event_list.append('cpu-cycles')
-	event_list.append('cache-references')
-	event_list.append('L1-dcache-loads')
-	event_list.append('L1-dcache-load-misses')
-	event_list.append('L1-dcache-stores')
-	event_list.append('L1-dcache-store-misses')
-	event_list.append('L1-dcache-prefetch-misses')
-	event_list.append('L1-icache-load-misses')
-	event_list.append('LLC-loads')
-	event_list.append('LLC-load-misses')
-	event_list.append('LLC-stores')
-	event_list.append('LLC-store-misses')
-	event_list.append('LLC-prefetches')
-	event_list.append('LLC-prefetch-misses')
-	event_list.append('dTLB-loads')
-	event_list.append('dTLB-load-misses')
-	event_list.append('dTLB-stores')
-	event_list.append('dTLB-store-misses')
-	event_list.append('iTLB-loads')
-	event_list.append('iTLB-load-misses')
-	event_list.append('branch-loads')
-	event_list.append('branch-load-misses')
-	event_list.append('node-loads')
-	event_list.append('node-load-misses')
-	event_list.append('node-stores')
-	event_list.append('node-store-misses')
-	event_list.append('node-prefetches')
-	event_list.append('node-prefetch-misses')
+def mean(lst):
+	return sum(lst)/float(len(lst))
 
-	for i in range(len(event_list)):
+def init_event_list():
+	event_list.append('uncore_imc_0/event=0x4,umask=0x3/')
+	event_list.append('uncore_imc_1/event=0x4,umask=0x3/')
+	event_list.append('uncore_imc_2/event=0x4,umask=0x3/')
+	event_list.append('uncore_imc_3/event=0x4,umask=0x3/')
+
+	for i in range(len(event_list)*SOCKET_NR):
 		event_value.append([])
 
-def execute_cmd(event_name, pid, output):
-	cmd = "perf stat -e " + event_name + " -p " + pid + " sleep "+ INTERVAL +  " 2>>" + output
+def execute_cmd(event_name, output):
+	cmd = "perf stat -e " + event_name + " -I" + INTERVAL + " -a --per-socket" + " sleep "+ TOTAL_TIME +  " 2>>" + output
 	os.popen(cmd)
 
 def process(output):
@@ -80,15 +49,16 @@ def process(output):
 
 	locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-	for i in range(len(event_list)):
-		for activity in activities:
-			activity_list = activity.split(' ')
-			for j in range(len(activity_list)):
-				if ((activity_list[j] == event_list[i]) or (activity_list[j] == event_list[i] + OPTIONS)):
+	for activity in activities:
+		activity_list = activity.split()
+		if (len(activity_list) == 5):
+			for i in range(len(event_list)):
+				if ((activity_list[4] == event_list[i])):
+					socket = (int)(activity_list[1].split("S")[1])
 					try:
-						event_value[i].append(locale.atoi(activity_list[j-1]))
+						event_value[socket*len(event_list)+i].append(locale.atoi(activity_list[3]))
 					except (TypeError, ValueError, AttributeError):
-						event_value[i].append(0)
+						event_value[socket*len(event_list)+i].append(0)
 
 def main(argv):
 	init_event_list()
@@ -98,7 +68,7 @@ def main(argv):
 	event_temp = ""
 	for event in event_list:
 		index = index + 1
-		event_temp = event_temp + event + OPTIONS + ","
+		event_temp = event_temp + event + ","
 		if (index == CONCURRENCY):
 			index = 0
 			event_name_list.append(event_temp[:-1])
@@ -109,34 +79,28 @@ def main(argv):
 	cmd = "rm -rf output"
 	os.popen(cmd)
 
-	for i in range(ROUND_NR):
-		for event_name in event_name_list:
-			execute_cmd(event_name, PID, "output")
+	for event_name in event_name_list:
+		execute_cmd(event_name, "output")
 
 	process("output")
 
 	result = open('data', 'a')
-	result.writelines("%d "% (median(item)) for item in event_value)
-	result.writelines("\n")
+	result.writelines("%d "% (mean(item)) for item in event_value)
+#	result.writelines("%d "% (median(item)) for item in event_value)
 	result.close()
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("pid", help="specify the process id")
 	parser.add_argument("-c", "--concurrency", type=int, default=50,
 			    help="specify how many counters concurrently")
-	parser.add_argument("-n", "--round_nr", type=int, default=5,
-			    help="specify how many rounds you want to counter")
-	parser.add_argument("-i", "--interval", default="0.1",
-			    help="specify how long for each round")
-	parser.add_argument("-o", "--options", default="ukG",
-			    help="what to measure for perf")
+	parser.add_argument("-t", "--time", default="10",
+			    help="specify the total time (s)")
+	parser.add_argument("-i", "--interval", default="1000",
+			    help="specify the interval (ms)")
 	args = parser.parse_args()
 
-	PID = args.pid
 	CONCURRENCY = args.concurrency
-	ROUND_NR = args.round_nr
+	TOTAL_TIME = args.time
 	INTERVAL = args.interval
-			    help="specify how long for each round")
-	parser.add_argument("-o", "--options", default="ukG",
+	main(sys.argv[1:])
