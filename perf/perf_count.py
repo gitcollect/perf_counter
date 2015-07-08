6fa5aba3d247df7ac702b2a97f338e0a04c6acc3
root <root@nebula2.com>
2015-07-08 15:45:18 -0400

    modify perf_count

--- perf/perf_count.py
+++ perf/perf_count.py
@@ -14,8 +14,8 @@ import argparse
 
 PID = ""
 CONCURRENCY = 0
-ROUND_NR = 0
 INTERVAL = ""
+TOTAL_TIME = ""
 OPTIONS = ""
 
 event_list = []
@@ -62,7 +62,7 @@ def init_event_list():
 		event_value.append([])
 
 def execute_cmd(event_name, pid, output):
-	cmd = "perf stat -e " + event_name + " -p " + pid + " sleep "+ INTERVAL +  " 2>>" + output
+	cmd = "perf stat -e " + event_name + " -I" + INTERVAL + " -p " + pid + " sleep "+ TOTAL_TIME +  " 2>>" + output
 	os.popen(cmd)
 
 def process(output):
@@ -73,15 +73,14 @@ def process(output):
 
 	locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-	for i in range(len(event_list)):
-		for activity in activities:
-			activity_list = activity.split(' ')
-			for j in range(len(activity_list)):
-				if ((activity_list[j] == event_list[i]) or (activity_list[j] == event_list[i] + OPTIONS)):
-					try:
-						event_value[i].append(locale.atoi(activity_list[j-1]))
-					except (TypeError, ValueError, AttributeError):
-						event_value[i].append(0)
+	for activity in activities:
+		activity_list = activity.split()
+		for i in range(len(event_list)):
+			if ((activity_list[2]== event_list[i])or(activity_list[2]==event_list[i]+OPTIONS)):
+				try:
+					event_value[i].append(locale.atoi(activity_list[1]))
+				except (TypeError, ValueError, AttributeError):
+					event_value[i].append(0)
 
 def main(argv):
 	init_event_list()
@@ -102,18 +101,14 @@ def main(argv):
 	cmd = "rm -rf output"
 	os.popen(cmd)
 
-	for i in range(ROUND_NR):
-		for event_name in event_name_list:
-			execute_cmd(event_name, PID, "output")
+	for event_name in event_name_list:
+		execute_cmd(event_name, PID, "output")
 
 	process("output")
 
 	result = open('data', 'a')
-#	for i in range(ROUND_NR):
-#		result.writelines("%d "% (item[i]) for item in event_value)
 	result.writelines("%d "% (mean(item)) for item in event_value)
 #	result.writelines("%d "% (median(item)) for item in event_value)
-#		result.writelines("\n")
 	result.close()
 
 if __name__ == "__main__":
@@ -122,17 +117,17 @@ if __name__ == "__main__":
 	parser.add_argument("pid", help="specify the process id")
 	parser.add_argument("-c", "--concurrency", type=int, default=50,
 			    help="specify how many counters concurrently")
-	parser.add_argument("-n", "--round_nr", type=int, default=5,
-			    help="specify how many rounds you want to counter")
-	parser.add_argument("-i", "--interval", default="0.1",
-			    help="specify how long for each round")
+	parser.add_argument("-t", "--time", default="10",
+			    help="specify the total time (s)")
+	parser.add_argument("-i", "--interval", default="1000",
+			    help="specify the interval time (ms)")
 	parser.add_argument("-o", "--options", default="uk",
 			    help="what to measure for perf")
 	args = parser.parse_args()
 
 	PID = args.pid
 	CONCURRENCY = args.concurrency
-	ROUND_NR = args.round_nr
+	TOTAL_TIME = args.time
 	INTERVAL = args.interval
 	OPTIONS = ":" + args.options
 	main(sys.argv[1:])
